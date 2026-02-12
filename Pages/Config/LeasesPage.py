3db83ae3ce868fbf749432fdaa93cdb3adfc296e
unknown <atulwanve03@gmail.com>
2026-02-12 17:17:40 +0530

    Replace local age parser with shared utility

--- Pages/Config/LeasesPage.py
+++ Pages/Config/LeasesPage.py
@@ -2,12 +2,12 @@
 Dynamic implementation of the Leases page with live Kubernetes data.
 """
 
-from PyQt6.QtWidgets import QHeaderView, QPushButton
+from PyQt6.QtWidgets import QHeaderView
 from PyQt6.QtCore import Qt, QTimer
 
 from Base_Components.base_components import SortableTableWidgetItem
 from Base_Components.base_resource_page import BaseResourcePage
-from UI.Styles import AppStyles
+from Utils.data_formatters import parse_age_to_seconds
 
 class LeasesPage(BaseResourcePage):
     """
@@ -32,7 +32,7 @@ class LeasesPage(BaseResourcePage):
         sortable_columns = {1, 2, 3, 4}
 
         # Set up the base UI components
-        layout = super().setup_ui("Leases", headers, sortable_columns)
+        super().setup_ui("Leases", headers, sortable_columns)
 
         # Configure column widths
         self.configure_columns()
@@ -98,11 +98,7 @@ class LeasesPage(BaseResourcePage):
 
             # Handle numeric columns for sorting
             if col == 3:  # Age column
-                try:
-                    num = int(value.replace('d', ''))
-                except ValueError:
-                    num = 0
-                item = SortableTableWidgetItem(value, num)
+                item = SortableTableWidgetItem(value, parse_age_to_seconds(value))
             else:
                 item = SortableTableWidgetItem(value)
 
