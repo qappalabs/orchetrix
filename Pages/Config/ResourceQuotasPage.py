5844889648319e62d27936499647c494c232ce62
unknown <atulwanve03@gmail.com>
2026-02-12 17:24:24 +0530

    Replace local age parser with shared utility

--- Pages/Config/ResourceQuotasPage.py
+++ Pages/Config/ResourceQuotasPage.py
@@ -2,12 +2,12 @@
 Dynamic implementation of the ResourceQuotas page with live Kubernetes data.
 """
 
-from PyQt6.QtWidgets import QHeaderView, QPushButton
+from PyQt6.QtWidgets import QHeaderView
 from PyQt6.QtCore import Qt, QTimer
 
 from Base_Components.base_components import SortableTableWidgetItem
 from Base_Components.base_resource_page import BaseResourcePage
-from UI.Styles import AppStyles
+from Utils.data_formatters import parse_age_to_seconds
 
 
 class ResourceQuotasPage(BaseResourcePage):
@@ -33,7 +33,7 @@ class ResourceQuotasPage(BaseResourcePage):
         sortable_columns = {1, 2, 3}
 
         # Set up the base UI components
-        layout = super().setup_ui("Resource Quotas", headers, sortable_columns)
+        super().setup_ui("Resource Quotas", headers, sortable_columns)
 
         # Configure column widths
         self.configure_columns()
@@ -96,11 +96,7 @@ class ResourceQuotasPage(BaseResourcePage):
 
             # Handle numeric columns for sorting
             if col == 2:  # Age column
-                try:
-                    num = int(value.replace('d', ''))
-                except ValueError:
-                    num = 0
-                item = SortableTableWidgetItem(value, num)
+                item = SortableTableWidgetItem(value, parse_age_to_seconds(value))
             else:
                 item = SortableTableWidgetItem(value)
 
